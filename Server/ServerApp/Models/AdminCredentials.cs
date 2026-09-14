a94f4c511fadab4f194c36c962d231d10e7d74be
Ivan <ivan.iliuk@chnu.edu.ua>
2026-09-14 21:12:19 +0300

    Add JWT authentication setup and admin credentials

---
+++ Server/ServerApp/Models/AdminCredentials.cs
@@ -0,0 +1,8 @@
+namespace ServerApp
+{
+    public static class AdminCredentials
+    {
+        public const string Login = "Love";
+        public const string Password = "Peach";
+    }
+}
--- Server/ServerApp/Program.cs
+++ Server/ServerApp/Program.cs
@@ -1,12 +1,39 @@
 using Microsoft.EntityFrameworkCore;
 using ServerApp.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var jwtKey = "PeachPeachPeachPeachPeachPeachPeach2026";
+var jwtIssuer = "EventManagementApp";
+var jwtAudience = "EventManagementAppUsers";
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
